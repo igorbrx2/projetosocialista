7edf2bde7eac7a4da9444ea37965d9656bb589d9
brx.igor2 <brx.igor@gmail.com>
2024-07-31 14:33:48 -0300

    Site atualizado com modais

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // MODAIS DE APRESENTAÇÃO
 
-    // conteúdo dos candidatos
+    // conteúdo dinâmico dos candidatos
     const candidatos = {
-        'nandoPoeta': `
-        <p>Meu nome é Nando Poeta. Sou sociólogo, tenho 61 anos e nasci em Natal.Sou casado, pai de três filhos e avô. Já fui professor da rede privada e da rede pública do Estado de São Paulo, onde lecionava Sociologia. Atualmente, trabalho na Secretaria de Educação do Rio Grande do Norte e no Instituto Latino-Americano de Estudos Socioeconômicos (Ilaese).Sou cordelista com vários títulos publicados, muitos com temática político-social e um dos organizadores do Ponto de Memória Estação do Cordel. <br>
+    'nandoPoeta': `
+    <p>Meu nome é Nando Poeta. Sou sociólogo, tenho 61 anos e nasci em Natal.Sou casado, pai de três filhos e avô. Já fui professor da rede privada e da rede pública do Estado de São Paulo, onde lecionava Sociologia. Atualmente, trabalho na Secretaria de Educação do Rio Grande do Norte e no Instituto Latino-Americano de Estudos Socioeconômicos (Ilaese).Sou cordelista com vários títulos publicados, muitos com temática político-social e um dos organizadores do Ponto de Memória Estação do Cordel. <br>
 
     <br>Iniciei minha militância política no movimento secundarista, na antiga Escola Técnica Federal. Ingressei no curso de Metalurgia, depois passei para o curso de Edificações. Foi nesse período que dei meus primeiros passos na luta estudantil, participando e apoiando uma chapa para o centro cívico. Fui trabalhar então na construção civil, onde tive contato com os operários e suas vivências. Em seguida, entrei para a Universidade Federal do Rio Grande do Norte (UFRN), onde conheci o movimento estudantil, no curso de Ciências Sociais. Integrei o Centro Acadêmico do meu curso e participei da ocupação da Reitoria em 1984.<br>
 
@@ -22,7 +22,104 @@
     <br>-	Fim da privatização e terceirização na saúde;<br>
     <br>-	Suspensão do pagamento da dívida pública e auditoria das contas do município;<br>
     <br>-	Criação de empresa municipal de transporte;<br>
+    </p>`,
+
+    'tiagoSilva': `<p>
+    Meu nome é Tiago, sou natural de Natal/RN e moro no bairro Bom Pastor, na zona Oeste da cidade. Sou jornalista na Poti Comunicação e estudante de Licenciatura em Teatro na Universidade Federal do Rio Grande do Norte (UFRN). Também sou poeta, escritor, ator e performer. Em 2019, lancei meu primeiro zine, “Sangria”, e tenho contos e crônicas publicados em sites. Faço parte do Movimento Slam Rima Central, que promove batalhas de poesia, e sou um dos fundadores do coletivo LGBTI Auroras.<br><br>Conheci o PSTU em 2014, durante o programa semestral de TV (que hoje não temos mais). Esse partido me chamou atenção pela sua defesa de um modelo diferente de sociedade, o socialismo, e então comecei a acompanhá-lo pela internet. Ainda em 2014, nas eleições gerais, votei pela primeira vez para presidente no candidato do partido, Zé Maria à época, e também nos demais candidatos da legenda. E, em 2016, passei a militar organicamente, onde estou até hoje.<br><br>O PSTU foi fundamental na minha vida. Foi o partido que ampliou minha visão de mundo.
+Passei então a entender que meu contexto social não era normal, como morar num bairro periférico estigmatizado e esquecido pelos governos, usar serviços públicos precários e  presenciar a violência urbana diária, com vários jovens da minha geração nos deixando. É para denunciar esses problemas da sociedade capitalista e para fortalecer a luta da nossa classe que estou junto com minhas companheiras e companheiros do PSTU nestas eleições.<br>
+
+<br><strong>Pontos do Programa:</strong><br>
+
+<br>-	Combater a LGBTIfobia e transfobia;<br>
+<br>-	Combater a privatização dos equipamentos culturais;<br>
+<br>-	Destinar mais recursos para políticas públicas de cultura;<br>
+<br>-	Dobrar o orçamento para a cultura;<br>
+<br>-	Fomentar o acesso da população aos equipamentos culturais públicos;<br>
+<br>-	Cobrar os grandes devedores do município e reverter esse dinheiro para os serviços públicos;<br>
+<br>-	Universalizar a educação básica;<br>
+<br>-	Passe livre para estudantes e desempregados;<br>
+<br>-	Desmilitarização da PM;<br>
+<br>-	Combater o genocídio da juventude das periferias.<br>
 </p>`,
+
+'ericaGuarani': `<p>
+  Eu sou Érica Guarani. Sou uma mulher indígena nascida em Natal/RN. Formei-me em Ciências Biológicas e sou técnica de enfermagem concursada. Enquanto trabalhadora da saúde municipal, tenho enfrentamentos diretos com a gestão truculenta do prefeito Álvaro Dias (Republicanos). Faço parte da direção do Sindsaúde/RN, adoro arte e me expresso através de desenhos, pinturas e textos.<br>
+  <br>Desde muito jovem na escola eu já me interessava por assuntos políticos e defendia os direitos dos (as) estudantes. Porém, comecei a me interessar especificamente pelas ideias socialistas e revolucionárias quando vi as propagandas do PSTU na TV. A partir daí, comecei a refletir melhor sobre a minha realidade e a do mundo, e entendi que os problemas da sociedade não eram naturais.<br>
+  <br>Em 2012, passei a ter contato de fato com o Partido, no período de pré-campanha. Nesse ano, o meu primeiro voto para presidente foi no Zé Maria, que era nosso candidato ao cargo à época. Depois, comecei a conhecer alguns (as) militantes do Partido e, em 2014, comecei a militar organicamente. Além do PSTU, construo o Movimento Mulheres em Luta (MML) e a CSP-Conlutas.<br>
+  <br>A pandemia da covid-19, durante a qual trabalhei na linha de frente como técnica de enfermagem, foi um dos períodos mais difíceis da minha vida. Além do medo de me contaminar e transmitir o vírus para minha família e amigos, vi muitas pessoas nos deixarem abruptamente. O pior de tudo foi presenciar o descaso dos governos com a saúde pública, pois muitas mortes poderiam ter sido evitadas. Mas esse episódio me fez entender ainda mais a necessidade urgente de destruir a sociedade capitalista. E a minha candidatura e a dos meus companheiros será porta-voz dessa ideia.<br>
+  
+  <br><strong>Pontos do programa:</strong><br>
+
+  <br>-	Combater o machismo e o racismo;<br>
+  <br>-	Ampliar a Lei Maria da Penha;<br>
+  <br>-	Defender os povos indígenas;<br>
+  <br>-	SUS 100% público;<br>
+  <br>-	Estatização dos serviços de saúde;<br>
+  <br>-	Fim da privatização e terceirização na saúde;<br>
+  <br>-	Estruturação das Unidades Básicas de Saúde;<br>
+  <br>-	Realização de concurso público para a saúde e contratar os cadastros de reserva.<br>
+</p>`,
+
+'alexandreGuedes': `<p>
+Sou Alexandre Guedes Fernandes, tenho 59 anos, sou casado e nasci em Natal. Tenho formação acadêmica em Ciências Sociais/Sociologia. Ingressei no serviço público, no Departamento Estadual de Trânsito do Rio Grande do Norte (Detran/RN), em 1984, aos 19 anos, há exatos 40 anos, de onde sou recém-aposentado. Entrei no movimento sindical em 1989, quando participei da fundação do Sinai, compondo sua direção. Hoje, integro a Coordenação Executiva do sindicato, do qual estou licenciado.<br>
+<br>Em 1999, ingressei nas fileiras do Partido Socialista dos Trabalhadores Unificado (PSTU), do qual sou militante desde então. Diante do governismo que tomou conta de boa parte do movimento sindical de nosso país, com o advento dos governos do PT, rompi com a Central única dos Trabalhadores (CUT) em 2004 e participei ativamente da construção da Conlutas (Coordenação Nacional de Lutas) em 2006. Em 2010, juntamente com vários segmentos independentes da classe trabalhadora, participei da construção da CSP-Conlutas – Central Sindical e Popular, da qual sou membro licenciado de sua Secretaria Executiva Estadual.<br>
+<br>Sempre estive nas lutas em defesa do serviço público, participando ativamente das campanhas pelos planos de carreira dos servidores, concurso público e combatendo a privatização do estado nas suas 3 esferas  - municipal, estadual e nacional.<br>
+<br>Defendo a organização da classe trabalhadora em conselhos populares. Neles há possibilidade de se ampliar a disputa pelo poder político e econômico nos municípios, confrontando prefeitos e vereadores, que depois das eleições têm o costume de transformar a Prefeitura e a Câmara Municipal  em balcão de negócios dos grandes empresários.<br>
+<br>Assim como Trotsky, líder revolucionário russo, defendo que “Toda Revolução é impossível até que se torne inevitável”. Para mim, a luta pelo socialismo é atual, pois a classe trabalhadora é a única que pode salvar a vida no planeta com organização e luta revolucionária. Quem produz a riqueza é quem deve governar.<br>
+
+<br><strong>Pontos do Programa:</strong><br> 
+
+<br>-	Cobrar os grandes devedores do município;<br>
+<br>-	Suspensão do pagamento da dívida pública e auditoria das contas do município;<br>
+<br>-	Criação de empresa municipal de transporte;<br>
+<br>-	Redução da jornada de trabalho sem redução do salário;<br>
+<br>-	Plano de obras públicas para empregar todos os desempregados;<br>
+<br>-	Universalização do saneamento básico.<br>
+<br>-	Passe livre para estudantes e desempregados;<br>
+<br>-	Contra o desmonte da Urbana;<br>
+<br>-	Combater a especulação imobiliária;<br>
+<br>-	Defender a mata atlântica e os manguezais;<br>
+</p>`,
+
+'joseJairan': `<p>
+  Eu sou José Jairan. Sou professor de Matemática com vínculo no estado e também sou formado em Letras. Construo o Movimento Nacional Quilombo Raça e Classe, o Movimento Muda Sinte e sou um dos fundadores do coletivo LGBTI Auroras.<br> 
+  <br>Para mim, o socialismo não é uma utopia; é uma necessidade. A situação de opressão e exploração que vive a classe trabalhadora não é normal. Pelo contrário, isso ocorre devido ao sistema capitalista. Enquanto homem negro, LGBTI e de terreiro, sou um dos principais alvos desta sociedade.<br> 
+  <br>É para defender nossos direitos e dar eco a essas ideias que construo o PSTU e tenho orgulho de dizer que sou revolucionário. Basta de opressão e exploração!<br>
+  
+  <br><strong>Pontos do Programa:</strong><br>
+
+  <br>-	Combater o racismo, a LGBTIfobia e a transfobia;<br>
+  <br>-	Defender os povos indígenas;<br>
+  <br>-	Combater o racismo religioso;<br>
+  <br>-	Igualar o salário dos políticos para o de uma professora municipal;<br>
+  <br>-	Ampliação da estrutura física das escolas;<br>
+  <br>-	Universalizar a educação básica;<br>
+  <br>-	5% do PIB de Natal para a educação.<br>
+  <br>-	Defender o piso do magistério.<br>
+  <br>-	Concurso público para a educação.<br>
+  <br>-	Defender acessibilidade e inclusão nas escolas e nos demais espaços públicos.<br>
+</p>`,
+
+'lucianaLima': `<p>
+Sou Luciana Lima, tenho 54 anos. Atuo como professora da rede municipal de educação de Natal e sou aposentada da rede estadual. Formei-me em Pedagogia pela Universidade Federal do Rio Grande do Norte (UFRN) e tenho especialização em Psicopedagogia. Sou filha de um ex-fuzileiro naval e de uma dona de casa. Nasci em Natal e cresci no bairro das Quintas. Estudei nas escolas públicas Professora Maria Lídia, Escola Rotary, e fiz o Magistério no Instituto de Educação Superior Presidente Kennedy (IFESP).<br>
+
+<br>Como todo (a)  filho e filha de trabalhadores (as), tive que trabalhar muito cedo para garantir pelo menos o ticket estudante para ir para a escola e para a universidade. Foi na universidade que comecei a compreender a importância da organização da classe trabalhadora. E quando me tornei funcionária pública, filiei-me ao Sindicato dos Trabalhadores em Educação do Rio Grande do Norte (Sinte/RN).<br>  
+
+<br>Comecei minha militância política com um pé na Convergência Socialista e o outro no partido que estava surgindo: o PSTU. De lá para cá, muitas histórias foram construídas. Alguns atropelos, percalços, enfrentamentos e muitas lutas. Mas nunca deixei de acreditar e de lutar para construir e fortalecer a ferramenta que considero imprescindível para a transformação social: o partido revolucionário. A revolução não está sob o controle do PSTU. Porém, este é o partido que coloca o seu programa e seus militantes a serviço da construção de uma sociedade socialista, sem exploração e opressão.<br>
+
+<br><strong>Pontos do Programa:</strong><br>
+
+<br>-	Combater o machismo;<br>
+<br>-	Ampliar a Lei Maria da Penha;<br>
+<br>-	Igualar o salário dos políticos para o de uma professora municipal;<br>
+<br>-	Ampliação da estrutura física das escolas;<br>
+<br>-	Universalizar a educação básica;<br>
+<br>-	5% do PIB de Natal para a educação.<br>
+<br>-	Defender o piso do magistério.<br>
+<br>-	Concurso público para a educação.<br>
+<br>-	Defender acessibilidade e inclusão nas escolas e nos demais espaços públicos.<br>
+</p>`,
+
     };
     
     document.addEventListener('DOMContentLoaded', () => {
@@ -36,18 +133,18 @@
               const idCandidato = candidato.getAttribute('data-candidato');
               if (candidatos[idCandidato]) {
                 modalConteudo.innerHTML = candidatos[idCandidato];
-                modalContainer.style.display = 'flex'; // Mostrar o modal
+                modalContainer.style.display = 'flex';
               }
             });
           });
 
           botaoFechar.addEventListener('click', () => {
-            modalContainer.style.display = 'none'; // Fechar o modal
+            modalContainer.style.display = 'none';
           });
         
           modalContainer.addEventListener('click', (event) => {
             if (event.target === modalContainer) {
-              modalContainer.style.display = 'none'; // Fechar o modal ao clicar fora
+              modalContainer.style.display = 'none';
             }
           });
         });
