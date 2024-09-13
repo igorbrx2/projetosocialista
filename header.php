bafc42306905b226eb8231aa59b2192aa18536ae
brx.igor2 <brx.igor@gmail.com>
2024-09-13 10:40:01 -0300

    atualizacoes do header

--- header.php
+++ header.php
@@ -4,6 +4,10 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="cache-control" content="no-cache, no-store, must-revalidate" />
+  <meta http-equiv="pragma" content="no-cache" />
+  <meta http-equiv="expires" content="0" />
+
   <title>Projeto Socialista</title>
   <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/global.css" />
   <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css" />
